48753ff1963ac4cefd594258f605af9aad68fa09
the-samarium <tropicalvoilet008@gmail.com>
2025-07-07 02:03:36 +0530

    Links added

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
         <div className='w-full border-2 rounded-lg p-4 flex justify-center items-center font-mono gap-6'>
         <div className="w-fit   p-5 flex items-start mr-10">
           <ol className='flex items-start flex-col gap-3 p-5 font-bold text-xl'>
-            <li><a href="">LinkedIn</a></li>
-            <li><a href="">Instagram</a></li>
-            <li><a href="">Twitter</a></li>
-            <li><a href="">Help</a></li>
+            <li><a target='_blank' href="https://www.linkedin.com/in/sameer-chavan-93004b2b5?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">LinkedIn</a></li>
+            <li><a target='_blank'href="https://www.instagram.com/thee.heisenberg?igsh=MTVmNjMxZTB0a3BxeA==">Instagram</a></li>
+            <li><a target='_blank'href="https://x.com/Thenames_sameer?t=3OJoWMOWV3k3vOPOhAWijw&s=09">Twitter</a></li>
+            <li><a target='_blank'href="https://github.com/the-samarium">Github</a></li>
           </ol>
         </div>
         
